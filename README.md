aa01a60d54db03bb8acb31a26a6e8f11c8bb2ff9
Riagi15 <erinvleal@gmail.com>
2020-09-16 02:07:49 -0700

    Pushing what I have for tonight, created html divs, mostly skeleton in the javascript with lots of screwing around. Need to sleep for the night, will sort out the trash in the morning.

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# basic-timetable
+basic-timetable
+Like the name implies this is a simple 24 hour based timetable for planning out one's day.
+
+Times slots are segmented into blocks, which change color based off the time of day in which you load the page.
+
+You can enter text into any of these blocks, and it will save locally so it can be recalled whenever you open the page back up.
+
+It also tells you the exact date and time when you start up the page, down to the second. Unnecessary? Probably, but I like it so there it is.
+
+(possible) bonus. Try putting the word "Sleep" in two segments, ex: 10 P.M. and 8 A.M.
--- script.js
+++ script.js
@@ -1,14 +1,31 @@
 // consts/vars
 
+let currentHour = moment().format('HH');
+//console.log (currentHour);
+
+let divHour = [
+  0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23];
+//console.log(divHour);
+
 //Displays today's date and time at the top of the planner, in the current day div.
 
 moment().format('MMMM Do YYYY, h:mm:ss a');
 
-$(document).ready(function(){
-    $('#currentDay').text(moment().format('MMMM Do YYYY, h:mm:ss a'));
-  });
+$(document).ready(function () {
+  $('#currentDay').text(moment().format('MMMM Do YYYY, h:mm:ss a'));
+});
 
 //Takes values assigned to the date divs in the html and assigns their colors based off the current time.
+
+//if (divHour === currentHour) {
+
+//add .present class via .addClass
+
+//} else if (divHour < currentHour) {
+
+//add .past class
+
+//} else //add .future class
 
 //Allows input in the fields created in the time slots.
 
